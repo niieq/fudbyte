e18027b7a9b23869f28b8a92f66ed32406f305a3
Niieq <nii.nmarley@gmail.com>
2018-09-22 13:58:16 +0000

    search in admin

--- fudbyte/apps/restaurant/admin.py
+++ fudbyte/apps/restaurant/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Restaurant, Food, Comment
 
+
+class FoodAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'restaurant', 'image')
+    search_fields = ('name', 'restaurant__name')
+
 admin.site.register(Restaurant)
-admin.site.register(Food)
+admin.site.register(Food, FoodAdmin)
 admin.site.register(Comment)
